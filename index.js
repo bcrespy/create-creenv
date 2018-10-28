3b55e575ed1b7a96403b546aacb1687b6084a73a
Baptiste Crespy <baptiste.crespy@gmail.com>
2018-10-28 22:51:44 +0100

    added option --nocomment and --mode

--- index.js
+++ index.js
@@ -1,27 +1,21 @@
 #!/usr/bin/env node
 
 /**
- * This project is distributed under the MIT license, you are free to 
- * use it however you want :) 
+ * @author Baptiste Crespy <baptiste.crespy@gmail.com>
+ * @license MIT
  * 
- * This CLI is for now really simple: it clones the repo on which the 
- * boilerplate is stored, clean the .git folder, run npm install into
- * the folder, and then ask for informations to update the package.json 
+ * This CLI is for now really simple: it clones the repo on which the boilerplate is stored, clean the .git folder, run 
+ * npm install into the folder, and then ask for informations to update the package.json 
  * 
- * This was only made to get the creenv-boilerplate on your computer
- * faster, nothing else. I'm pretty sure there are some cases i'm not 
- * covering, so if you encounter an error, feel free to report it to 
- * the git repo on this project so that we can work on it. 
+ * This was only made to get the creenv-boilerplate on your computer faster, nothing else. I'm pretty sure there are some cases 
+ * i'm not covering, so if you encounter an error, feel free to report it to the git repo on this project so that we can work 
+ * on it. 
  * 
- * The goal on creative environment is to make the process of setting 
- * up an es6 environment easy, by doing the less possible. This way 
- * you are still free you make your own choices regarding on the 
- * developement of your app. 
+ * The goal on creative environment is to make the process of setting up an es6 environment easy, by doing the less possible. 
+ * This way you are still free you make your own choices regarding on the developement of your app. 
  * 
- * [https://github.com/bcrespy/create-creenv](repo)
- * [https://github.com/bcrespy/create-creenv/issues](report)
+ * <https://github.com/bcrespy/create-creenv>
  * 
- * Originally developed by Baptiste Crespy 
  * ---------
  * Contributors 
  * My mum :(
@@ -35,11 +29,17 @@ var package         = require('./package.json');
 var git             = require('simple-git')();
 var spawn           = require('cross-spawn');
 var readline        = require('readline-sync');
+var readdir         = require('recursive-readdir');
+var uncomment       = require('decomment');
 var exec            = require('child_process').exec;
 
 var SPACES          = "                             ";
-var REPO            = "https://github.com/bcrespy/creenv-boilerplate.git";
 var DEFAULT_FOLDER  = "creenv";
+var REPOS           = {
+                        default: "https://github.com/bcrespy/creenv-boilerplate.git",
+                        demo: "https://github.com/bcrespy/creenv-boilerplate-demo.git",
+                        light: "https://github.com/bcrespy/creenv-boilerplate-light.git"
+                      };
 
 
 
@@ -89,13 +89,14 @@ function createFolder (folder) {
  * folder. 
  * 
  * @param {string} distFolder the folder in which to clone
+ * @param {string} repo url to the distant repo 
  * 
  * @return {Promise} resolve when repo is cloned
  */
-function getRepo (distFolder) {
+function getRepo (distFolder, repo) {
   return new Promise(function (resolve, reject) {
     console.log("fetching the repo");
-    git.clone(REPO, distFolder, null, function (arg) {
+    git.clone(repo, distFolder, null, function (arg) {
       if (arg === null) {
         resolve();
       } else {
@@ -119,15 +120,16 @@ function install (dest) {
     process.chdir(dest);
     var child = exec('npm install',
     function (error, stdout, stderr) {
-        console.log(chalk.grey(stdout));
-        console.log(chalk.grey(stderr));
-        if (error !== null) {
-          console.log(error);
-          reject();
-        } else {
-          resolve();
-        }
-        separation();
+      console.log(chalk.grey(stdout));
+      console.log(chalk.grey(stderr));
+      process.chdir("../");
+      if (error !== null) {
+        console.log(error);
+        reject();
+      } else {
+        resolve();
+      }
+      separation();
     });
   })
 }
@@ -142,7 +144,7 @@ function install (dest) {
 function removeGitRepo () {
   return new Promise((resolve, reject) => {
     console.log("removing the git repo currently on the project");
-    rimraf('/.git', function (error) {
+    rimraf('.git', function (error) {
       resolve(error === null ? 0 : 1);
     }); 
   });
@@ -155,29 +157,54 @@ function removeGitRepo () {
  * 
  * @param {object} json the JSON object from the package.json
  * @param {string} folder the folder path in which the package was from
+ * 
+ * @return {Promise} resolves if update is valid 
  */
 function updateJson (json, folder) {
-  var answer = null;
+  return new Promise(function (resolve, reject) {
+    var answer = null;
 
-  answer = readline.question("What's the name of your project (if empty, "+folder+" will be used) ? ");
-  json.name = answer === "" ? folder : answer;
+    answer = readline.question("What's the name of your project (if empty, "+folder+" will be used) ? ");
+    json.name = answer === "" ? folder : answer;
 
-  answer = readline.question("Description of your project (optional): ");
-  json.description = answer;
+    answer = readline.question("Description of your project (optional): ");
+    json.description = answer;
 
-  answer = readline.question("Your name (will be saved in the package.json) ? ");
-  json.author = answer;
+    answer = readline.question("Your name (will be saved in the package.json) ? ");
+    json.author = answer;
 
-  json.version = "0.0.1";
-  json.repository = {};
-  json.bugs = {};
-  json.homepage = {};
+    json.version = "0.0.1";
+    json.repository = {};
+    json.bugs = {};
+    json.homepage = {};
 
-  fs.writeFile("./package.json", JSON.stringify(json, null, 2), function (err) {
-    if (err) {
-      console.log(chalk.bold.red("\n\ncould not write the package.json, check it if you want to update it"))
-    }
-    console.log(chalk.bold.green("\n\nyour project is ready to be used. have a good time :)\n\n"));
+    fs.writeFile(folder+"/package.json", JSON.stringify(json, null, 2), function (err) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+/**
+ * Remove all the comments from all the javascript files within the /src folder, recusively 
+ * 
+ * @param {string} dest the installation destination folder
+ * 
+ * @return {Promise}
+ */
+function stripComments (dest) {
+  return new Promise( function (resolve, reject) {
+    readdir(dest+"/src", function (err, files) {
+      files.forEach(function (file) {
+        let content = fs.readFileSync(file, "utf8");
+        let replaced = uncomment(content);
+        fs.writeFileSync(file, replaced, { flag: "w" })
+      });
+      resolve();
+    });
   });
 }
 
@@ -185,18 +212,22 @@ function updateJson (json, folder) {
 program
   .version(package.version)
   .arguments('<dest>')
-  .option('-o, --open', 'if you want to enter the folder after installation')
+  .option('-m, --mode <mode>', 'the type of project you want to install default|demo|light')
+  .option('-n, --nocomment', 'remove all the comments within the js files')
   .option('-v, --version', 'display the current version of the cli')
-  .option('-V, --VERSION <version>', 'fetch a specific version of the creative environment')
   .action(function (dest, cmd) {
     if (dest === undefined) dest = DEFAULT_FOLDER;
+    
+    // if mode as argument, repo matches the mode 
+    let repo = REPOS[cmd.mode] !== undefined ? REPOS[cmd.mode] : REPOS["default"];
+
     startInfos();
     createFolder(dest);
-    getRepo(dest).then(function(){
+    getRepo(dest, repo).then(function () {
       console.log(chalk.bold.green("repo was sucessfully cloned"));
       separation();
 
-      install(dest).then(function(){
+      install(dest).then(function () {
         console.log(chalk.green("npm install done, node modules were installed"));
   
         removeGitRepo().then(function (ret) {
@@ -208,11 +239,25 @@ program
           separation();
   
           // loads the json file 
-          var packageJSON = require(process.cwd()+"/package.json");
+          var packageJSON = require(process.cwd()+"/"+dest+"/package.json");
+          
           // updates the package.json of the destination folder 
-          updateJson(packageJSON, dest);
-        })
-      }).catch(function(error){
+          updateJson(packageJSON, dest).then(function () {
+            separation();
+
+            // if asked, we remove the comments from the /src folder 
+            if (cmd.nocomment) {
+              stripComments(dest).then(function () {
+                console.log(chalk.bold.green("\n\nyour project is ready to be used. have a good time :)\n\n"));
+              });
+            } else {
+              console.log(chalk.bold.green("\n\nyour project is ready to be used. have a good time :)\n\n"));
+            }
+          }).catch(function (error) {
+            console.log(chalk.bold.red("\n\ncould not write the package.json, check it if you want to update it"));
+          });
+        });
+      }).catch(function (error) {
         console.log(chalk.bold.red("installation failed. the project is still in its early development so please report the conditions in which this error occured, thanks :)"))
         console.log("\n\n"+error+"\n\n");
       });
